2a27d2b83f6634eac02885ccfdea1935516d9d4c
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2023-08-04 18:32:53 -0400

    done: code for converting column to numeric using parse_number next: apply to all columns in both data frames

--- Code.R
+++ Code.R
@@ -4,8 +4,19 @@ kg1.df <- raw.df |>
 kg2.df <- raw.df |>
   select(state, state_abbrev, has_kingap, pct_guardexit_agelt_1, pct_guardexit_age1_to_5,pct_guardexit_age6_to_10,pct_guardexit_age11_to_15,pct_guardexit_age16_to_20)
 
-summary_kg.df <- kg1.df |>
-  # ages = c(pct_guardexit_agelt_1, pct_guardexit_age1_to_5,pct_guardexit_age6_to_10,pct_guardexit_age11_to_15,pct_guardexit_age16_to_20) |>
-  group_by(has_kingap) |>
-  summarize(mean = mean(pct_guardexit_agelt_1, na.rm = TRUE)
-            )
+#summary_kg.df <- kg1.df |>
+  #group_by(has_kingap) |>
+  #summarize(mean = mean(pct_guardexit_agelt_1, na.rm = TRUE)
+            #)
+
+str(kg1.df$pct_guardexit_agelt_1) #checking value type in col
+as.numeric(kg1.df$pct_guardexit_agelt_1) #attempt to convert to numeric produces NA because of % sign
+
+# https://stackoverflow.com/questions/8329059/how-to-convert-character-of-percent-into-numeric-in-r
+
+ages = c(pct_guardexit_agelt_1, pct_guardexit_age1_to_5,pct_guardexit_age6_to_10,pct_guardexit_age11_to_15,pct_guardexit_age16_to_20) |>
+
+kg1.df <- kg1.df |>
+  mutate(agelt_1_num = parse_number(pct_guardexit_agelt_1))
+  
+mean(kg1.df$pct_guardexit_agelt_1, na.rm = TRUE)
